36cea4f94687e4ce85452a1bbe8c7b0c94d05490
Hari nivasaa V <harinivasaa@gmail.com>
2024-07-31 16:17:17 +0530

    Delete src/app/app.routes.ts

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-    {path : '', loadComponent:()=> import('./login/login.component').then(a=>a.LoginComponent)},
-    {path : 'sidenav', loadComponent:()=> import('./sidenav/sidenav.component').then(a=>a.SidenavComponent)}, 
-    {path : 'dashboard', loadComponent:()=> import('./dashboard/dashboard.component').then(a=>a.DashboardComponent)} 
-
-];
